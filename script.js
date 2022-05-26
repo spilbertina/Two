0ddab87626d68b178ef863a47144a38b1583d7eb
spilbertina <spilbertina@gmail.com>
2022-05-26 18:10:23 +0300

    addition and multiplication in different functions

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
 const first = document.querySelector('.first');
 const second = document.querySelector('.second');
-const button = document.querySelector('.button');
+const equals = document.querySelector('.equals');
 const result = document.querySelector('.result');
+const cleare = document.querySelector('.cleare');
 
+const firstMult = document.querySelector('.first-mult');
+const secondMult = document.querySelector('.second-mult');
+const equalsMult = document.querySelector('.equals-mult'); 
+const resultMult = document.querySelector('.result-mult'); 
+const cleareMult = document.querySelector('.cleare-mult');
 
-button.addEventListener('click', sum);
 function sum() {
     let firstInput = +first.value;
     let secondInput = +second.value;
 
     result.textContent = firstInput + secondInput;
 }
+
+function del() {
+    first.value = '';
+    second.value = '';
+
+    result.textContent = '';
+}
+
+function multiplication() {
+    
+       resultMult.textContent = +firstMult.value * +secondMult.value;
+}
+
+function delMult() {
+    firstMult.value = '';
+    secondMult.value = '';
+
+    resultMult.textContent = '';
+}
+
+equals.addEventListener('click', sum);
+cleare.addEventListener('click', del);
+equalsMult.addEventListener('click', multiplication);
+cleareMult.addEventListener('click', delMult);
